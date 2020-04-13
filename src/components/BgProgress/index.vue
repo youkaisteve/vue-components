a9034fd0e280d23e4d9e6260541d161d1746d085
youkaisteve <youkaisteve@gmail.com>
2020-04-13 10:55:34 +0800

    fix style

---
+++ src/components/BgProgress/index.vue
@@ -0,0 +1,61 @@
+<!--
+/**
+ * 背景进度条
+ * @desc 利用背景色作为进度条
+ * @author steve.k.you
+ * @date 2020年4月10日
+ */
+-->
+<template>
+  <div style="position:relative;">
+    <div class="progress-bar" :style="style" />
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: 'bg-progress',
+  props: {
+    percentage: {
+      type: [Number, String],
+      default: 0
+    },
+    color: {
+      type: String,
+      default: '#E9F3FF'
+    }
+  },
+  data() {
+    return {
+      width: this.percentage
+    }
+  },
+  watch: {
+    percentage: function(val) {
+      if (val >= 0) {
+        this.width = val
+      } else {
+        this.width = 0
+      }
+    }
+  },
+  computed: {
+    style: function() {
+      return {
+        'background-color': this.color,
+        width: this.width + '%',
+        'z-index': -999
+      }
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.progress-bar {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+</style>
