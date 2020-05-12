10f8036658af9b781971115ba3ec70594ca49efc
youkaisteve <youkaisteve@gmail.com>
2020-05-12 09:46:16 +0800

    fix

--- src/views/quality.vue
+++ src/views/quality.vue
@@ -14,7 +14,12 @@
             <div class="create-time ft12">创建时间：2020/03/17 11:22</div>
           </div>
         </div>
-        <div class="qr-code">qr-code</div>
+        <div class="qr-code">
+          <div class="code">
+            <img src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes" />
+          </div>
+          <div>编号：1111</div>
+        </div>
       </div>
       <div class="check-form">
         <div class="title">质量检查</div>
@@ -109,13 +114,88 @@
           </div>
         </el-tab-pane>
         <el-tab-pane label="整改信息">
-          <div class="rectify">rectify</div>
+          <div class="rectify">
+            <div class="panel">
+              <el-row class="section">
+                <div class="title">第一次整改</div>
+                <el-col :span="12">
+                  <el-form>
+                    <el-form-item label="处理情况：">已按要求整改</el-form-item>
+                    <el-form-item label="整改人：">才华</el-form-item>
+                    <el-form-item label="整改时间：">2020-03-08</el-form-item>
+                    <el-form-item label="整改照片："></el-form-item>
+                  </el-form>
+                  <div class="photos">
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                  </div>
+                </el-col>
+                <el-col :span="12">
+                  <el-form>
+                    <el-form-item label="复检意见：">已按要求整改</el-form-item>
+                    <el-form-item label="复检人：">才华</el-form-item>
+                    <el-form-item label="复查时间：">2020-03-08</el-form-item>
+                    <el-form-item label="复验照片："></el-form-item>
+                  </el-form>
+                  <div class="photos">
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                    <img
+                      src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes"
+                    />
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
+          </div>
         </el-tab-pane>
         <el-tab-pane label="检查照片">
-          <div class="photos">photos</div>
+          <div class="photos">
+            <img src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes" />
+            <img src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes" />
+            <img src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes" />
+            <img src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes" />
+            <img src="https://oss.yzw.cn/889ca6a9-fbbc-4ca1-92bc-98480e6ca502.jpg?v=null&oss=yes" />
+          </div>
         </el-tab-pane>
         <el-tab-pane label="模型视点">
-          <div class="view-point">view-point</div>
+          <div class="view-point">
+            <div class="flex-row justify-between">
+              <div>
+                <span class="title">选择模型：</span>
+                <el-select size="medium">
+                  <el-option>111</el-option>
+                  <el-option>222</el-option>
+                  <el-option>333</el-option>
+                </el-select>
+              </div>
+              <el-button size="medium">选定构件</el-button>
+            </div>
+            <div class="model-container" id="bimViewPoint"></div>
+          </div>
         </el-tab-pane>
       </el-tabs>
     </div>
@@ -124,12 +204,12 @@
 
 <style lang="less" scoped>
 .quality-check-detail {
+  color: #091732;
   padding: 0 42px;
   .top {
     .head {
       .desc {
         margin-left: -25px;
-        color: #091732;
         padding: 24px 0;
       }
       .status {
@@ -155,9 +235,6 @@
       }
       .problem {
         margin-top: 17px;
-        span {
-          color: #091732;
-        }
         .level {
           margin-left: 60px;
           width: 43px;
@@ -170,9 +247,8 @@
       }
       .form {
         margin-top: 10px;
-        color: #091732;
         /deep/.el-form-item {
-          margin-bottom: 12px;
+          margin-bottom: 0;
           min-width: 230px;
         }
       }
@@ -212,8 +288,6 @@
             .user-date {
               font-size: 12px;
               color: #2d3950;
-              .user {
-              }
               .date {
                 margin-left: 8px;
               }
@@ -223,10 +297,35 @@
       }
     }
     .rectify {
+      .panel {
+        /deep/.el-form-item {
+          margin-bottom: 0;
+        }
+        .section {
+          .photos {
+            img {
+              width: 123px;
+              height: 108px;
+              padding: 10px 30px 0 0;
+            }
+          }
+        }
+      }
     }
     .photos {
+      img {
+        width: 123px;
+        height: 108px;
+        padding: 10px 30px 0 0;
+      }
     }
     .view-point {
+      .model-container {
+        min-height: 512px;
+        border-radius: 6px;
+        border: 1px solid #f6f8fa;
+        margin-top: 38px;
+      }
     }
   }
 }
